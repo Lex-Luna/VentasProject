23aad089a6ac8ae21eaec25b2f724eb5a57e1a31
Lex Luna <alexisgabriel.lu@gmail.com>
2024-10-28 11:31:46 -0500

    instalacion de material y creacion dee componente Home y cliente

--- AppVentas/src/app/app.component.ts
+++ AppVentas/src/app/app.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatTableModule } from '@angular/material/table';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatCardModule } from "@angular/material/card";
+import { RouterModule } from '@angular/router';
+
+import { MatButtonModule } from '@angular/material/button'; // Cambiar MatButton por MatButtonModule
+import { MatListModule } from '@angular/material/list';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatSidenavModule,
+    RouterModule,
+    MatButtonModule,
+    MatListModule
+    
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+[x: string]: any;
   title = 'AppVentas';
 }
--- AppVentas/src/app/app.routes.ts
+++ AppVentas/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { HomeComponent } from '../Component/home/home.component';
+import { ClienteComponent } from '../Component/cliente/cliente.component';
+export const routes: Routes = [
+    { path: "home", component: HomeComponent },
+    { path: "", redirectTo: 'home', pathMatch: "full" },
+    { path: "cliente", component: ClienteComponent }
+];
